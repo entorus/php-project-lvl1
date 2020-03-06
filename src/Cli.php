5d702934601861804d9a05733f5ae2730f92243c
Entorus <entorus@yandex.ru>
2020-03-06 23:02:25 +0900

    step 5 start

--- src/Cli.php
+++ src/Cli.php
@@ -8,6 +8,13 @@ use function cli\prompt;
 function run()
 {
     line('Welcome to the Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no".' . "\n");
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Hello, %s! \n", $name);
+
+    for ($i=0; $i < 3; $i++) { 
+        $number = random_int( 0, 100 );
+        $answer = prompt('Question: ' . $number);
+        line("Your answer: %s! \n", $answer);
+    }
 }
